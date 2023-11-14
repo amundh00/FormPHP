a06b175856160ff608f7bb67ee77fc36ed6a001f
amundh00 <amuhal00992@stud.noroff.no>
2023-11-14 10:12:09 +0100

    form

---
+++ form-handler.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h1>Takk for at du sendte inn</h1>
+<?php
+echo "Dett er fra php"
+echo var_dump($_GET;)
+?>
+    <p><a href=index.html>Back</a></p>
+</body>
+</html>
+

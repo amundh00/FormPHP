1109e2f289e51d974c7b9657a79b3598141c699e
amundh00 <amuhal00992@stud.noroff.no>
2023-11-14 10:54:38 +0100

    form php

--- form-handler.php
+++ form-handler.php
@@ -7,10 +7,35 @@
 </head>
 <body>
 
-<h1>Takk for at du sendte inn</h1>
+    <h1>Takk for at du sendte inn</h1>
 <?php
-echo "Dett er fra php"
-echo var_dump($_GET;)
+
+#echo "Dett er fra php: ";
+#echo var_dump($_GET);
+#echo var_dump($_POST);
+
+$msg = "";
+$sender ="";
+$to = "amundhalgunset@gmail.com";
+if ($_POST) {
+    #echo "<br>Det er data her...";
+    foreach($_POST as $key => $value) {  
+        $msg .= "\t"."$key: $value"."\r\n";
+        if ($key == "email"){
+            $sender = $value;
+        }  
+    }
+}
+#echo var_dump($msg);
+
+if ($msg && $sender) {
+    echo "<p>Skal prøve å sende mail!";
+    $msg = "Dette ble sendt inn: "."\r\n".$msg;
+    mail($to, $sender, $msg);
+}   else{
+    echo "<p>Beklager, Kunne ikke sende mail, prøv igjen!";
+}
+
 ?>
     <p><a href=index.html>Back</a></p>
 </body>
